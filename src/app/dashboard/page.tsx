4266e2a4c3334d0d39af7f09cc2d64fb166603ca
bbissing <bbissing101@gmail.com>
2023-10-08 13:25:34 -0500

    additional edits

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
+import Button from '../../components/ui/Button'
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
+  return <Button >hello</Button>
 }
 
 export default page
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react'
-import { cva } from 'class-variance-authority'
-
-interface ButtonProps {}
+import { ButtonHTMLAttributes, FC } from 'react'
+import { cva, VariantProps } from 'class-variance-authority'
+import { Loader2 } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
   'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
@@ -19,13 +19,31 @@ const buttonVariants = cva(
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
+      size:'default',
     },
   }
 )
 
-const Button: FC<ButtonProps> = ({}) => {
-  return <div>Button</div>
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean
+}
+
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  size,
+  isLoading,
+  ...props
+}) => {
+  return (
+    <button className={cn(buttonVariants({variant, size, className}))} disabled={isLoading} {...props}>
+      {isLoading ? <Loader2 className='mr-2 h-4 w-4 animate-spin' /> : null}
+      {children}
+    </button>
+  )
 }
 
 export default Button
