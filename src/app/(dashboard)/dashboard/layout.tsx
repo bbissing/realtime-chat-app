998fda25990c5fde5685d6085a3b19785ec85b9f
bbissing <bbissing101@gmail.com>
2023-11-11 20:00:04 -0700

    final edits

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -47,7 +47,7 @@ const Layout = async ({ children }: LayoutProps) => {
 
       <div className='max-md:hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
         <Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
-          <Icons.Logo className='h-8 w-auto text-indigo-600' />
+          <Icons.Logo className='h-8 w-auto text-blue-500' />
         </Link>
 
         {friends.length > 0 ? (
@@ -73,9 +73,9 @@ const Layout = async ({ children }: LayoutProps) => {
                     <li key={option.id}>
                       <Link
                         href={option.href}
-                        className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                        className='text-gray-700 hover:text-blue-500 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'
                       >
-                        <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
+                        <span className='text-gray-400 border-gray-200 group-hover:border-blue-500 group-hover:text-blue-500 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
                           <Icon className='h-4 wi-4' />
                         </span>
 
--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -85,10 +85,10 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ sessionId, friends }) => {
             sessionId,
             friend.id
             )}`}
-            className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'>
+            className='text-gray-700 hover:text-blue-500 hover:bg-gray-50 group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'>
             {friend.name}
             {unseenMessagesCount > 0 ? (
-              <div className='bg-indigo-600 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center'>
+              <div className='bg-blue-500 font-medium text-xs text-white w-4 h-4 rounded-full flex justify-center items-center'>
                 {unseenMessagesCount}
               </div>
             ) : null}
